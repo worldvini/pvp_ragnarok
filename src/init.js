7a34f4ce4204cad511fef225241fe44e1e9a7d8c
Vinicius S Fleury <worldvini@hotmail.com>
2022-08-06 23:13:42 -0300

    ajustando tempo de consulta

--- src/init.js
+++ src/init.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 
 const PoringApi = require("./poring_api");
 
-
 async function buscandoEternamente (){    
     const poringApi = new PoringApi( {} );
 
@@ -20,43 +19,45 @@ async function buscandoEternamente (){
     let pvprank = {}
     fs.readFile('./data/pvprank.json', (err, data) => {
         if (err) throw err;
+        
         pvprank = JSON.parse(data);
-    });
+        
+        console.log("atualizando dados de rank com dados da api poring world");
 
-    console.log("atualizando dados de rank com dados da api poring world");
-    while( resultado.length ){
-        const rankPerson = resultado.shift();
-
-        if( !pvprank[ rankPerson.charid ] ){
-            const padrao = {            
-                name: rankPerson.name,
-                rank: rankPerson.rank,
-                score: rankPerson.score,
-                erank: rankPerson.erank,
-                profession: rankPerson.profession,
-                professionIcon: rankPerson.professionIcon,
-                charid: rankPerson.charid,
-                level: rankPerson.level,
-                guildname: rankPerson.guildname,
-                situation: "same",
-                history: [{rank: rankPerson.rank, score: rankPerson.score, timestamp: timeNow}]
+        while( resultado.length ){
+            const rankPerson = resultado.shift();
+    
+            if( !pvprank[rankPerson.charid] ){
+                const padrao = {            
+                    name: rankPerson.name,
+                    rank: rankPerson.rank,
+                    score: rankPerson.score,
+                    erank: rankPerson.erank,
+                    profession: rankPerson.profession,
+                    professionIcon: rankPerson.professionIcon,
+                    charid: rankPerson.charid,
+                    level: rankPerson.level,
+                    guildname: rankPerson.guildname,
+                    situation: "same",
+                    history: [{rank: rankPerson.rank, score: rankPerson.score, timestamp: timeNow}]
+                }
+    
+                pvprank[rankPerson.charid] = padrao;
             }
-
-            pvprank[ rankPerson.charid ] = padrao;
+            
+            if( pvprank[ rankPerson.charid ].rank != rankPerson.rank || pvprank[ rankPerson.charid ].score != rankPerson.score ){
+                pvprank[ rankPerson.charid ]['situation'] =  pvprank[ rankPerson.charid ]['rank'] > rankPerson.rank ? "down" : pvprank[ rankPerson.charid ]['rank'] < rankPerson.rank ? "up" : "same";
+                pvprank[ rankPerson.charid ]['rank'] = rankPerson.rank;
+                pvprank[ rankPerson.charid ]['score'] = rankPerson.score;
+                pvprank[ rankPerson.charid ]['history'].push( {rank: rankPerson.rank, score: rankPerson.score, timestamp: timeNow } );
+            }
         }
-
-        if( pvprank[ rankPerson.charid ].rank != rankPerson.rank || pvprank[ rankPerson.charid ].score != rankPerson.score ){
-            pvprank[ rankPerson.charid ]['situation'] =  pvprank[ rankPerson.charid ]['rank'] > rankPerson.rank ? "down" : pvprank[ rankPerson.charid ]['rank'] < rankPerson.rank ? "up" : "same";
-            pvprank[ rankPerson.charid ]['rank'] = rankPerson.rank;
-            pvprank[ rankPerson.charid ]['score'] = rankPerson.score;
-            pvprank[ rankPerson.charid ]['history'].push( {rank: rankPerson.rank, score: rankPerson.score, timestamp: timeNow } );
-        }
-    }
-
-    console.log("Atualizando rank local - reescrevendo arquivo");
-    const newRank = JSON.stringify(pvprank);
-    fs.writeFileSync('./data/pvprank.json', newRank);
-    console.log("processo finalizado com sucesso");
+    
+        console.log("Atualizando rank local - reescrevendo arquivo");
+        const newRank = JSON.stringify(pvprank);
+        fs.writeFileSync('./data/pvprank.json', newRank);
+        console.log("processo finalizado com sucesso");
+    });
 }
 
 buscandoEternamente()
